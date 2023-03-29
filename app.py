22939ec56d9e4d02c10a0af73929bc81ffa589e9
limseulki <limslki333@hanmail.com>
2023-03-29 20:42:05 +0900

    레시피 수정작업 진행중 임시 커밋

--- app.py
+++ app.py
@@ -2,11 +2,13 @@
 from flask import Flask
 from route import recipe
 from route import review
+from route import user
 
 app = Flask(__name__)
 
 app.register_blueprint(recipe.blue_recipe)
 app.register_blueprint(review.blue_review)
+app.register_blueprint(user.blue_user)
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
--- route/recipe.py
+++ route/recipe.py
@@ -1,6 +1,7 @@
 # 레시피 전체 조회, 등록, 삭제, 수정
 from flask import Blueprint, Flask, render_template, request, jsonify
 app = Flask(__name__)
+
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://sparta:test@cluster0.er12y5s.mongodb.net/?retryWrites=true&w=majority')
 # client = MongoClient('mongodb+srv://sparta:test@cluster0.cirioky.mongodb.net/?retryWrites=true&w=majority')
@@ -17,15 +18,23 @@ blue_recipe = Blueprint("recipe", __name__, url_prefix="/recipe")
 def home():
    return render_template('main.html')
 
-# 없어도 될 것 같아요
+# main listing에서 필요함
 @blue_recipe.route('/list', methods=['GET'])
 def test_get():
-   all_recipes = list(db.recipes.find({},{'_id':False}))
+   # all_recipes = list(db.recipes.find({},{'_id':False}))
+   # return jsonify({'result':all_recipes})
+
+   all_recipes = []
+   recipes = list(db.recipes.find({}))
+   for recipe in recipes:
+      recipe['_id'] = str(recipe['_id'])
+      all_recipes.append(recipe)
+
    return jsonify({'result':all_recipes})
 
 # 레시피 등록 get
 @blue_recipe.route('/update', methods=['GET'])
-def test_update():
+def recipe_update():
    return render_template('update.html')
 
 # 레시피 등록 post
@@ -57,24 +66,49 @@ def recipe_post():
 def recipe_delete():
    name_receive = request.json['button_delete']
    db.recipes.delete_one({'name':name_receive})
+
+   # id_receive = request.form['id']
+   # under_id_receive = request.form['under_id']
+   # print(id_receive)
+   # print(under_id_receive)
+
    return jsonify({'msg':'삭제완료!'})
 
+# 레시피 수정 get
+@blue_recipe.route('/put/<under_id>')
+def get_modify(under_id):
+   print(under_id)
+   recipe = db.recipes.find_one({'_id':ObjectId(under_id)})
+   print(recipe)
+   return render_template('update.html', recipe=recipe)
+
 #레시피 수정 put
-@blue_recipe.route('/put', methods=['PUT'])
-def recipe_put():
-   name_receive = request.json['button_modify']
-   image_receive = request.json['image_give']
-   recipe_receive = request.json['recipe_give']
+# @blue_recipe.route('/put', methods=['PUT'])
+# def recipe_put():
+#    name_receive = request.json['name_give']
+#    image_receive = request.json['image_give']
+#    recipe_receive = request.json['recipe_give']
 
-   print(name_receive, image_receive, recipe_receive)
-   # db.recipes.update_one({'name':name_receive},{'$set':{'name':name_receive}})
-   # db.recipes.update_one({'name':name_receive},{'$set':{'image':image_receive}})
-   # db.recipes.update_one({'name':name_receive},{'$set':{'recipe':recipe_receive}})
-   # print(name_receive, image_receive, recipe_receive)
-   return jsonify({'msg':'수정완료!'})
+#    print(name_receive, image_receive, recipe_receive)
 
+#    doc = {
+#          'name': name_receive,
+#          'image': image_receive,
+#          'recipe': recipe_receive
+#       }
+   
+#    db.recipes.update_one({'name':name_receive},{'$set':doc})
+#    return jsonify({'msg':'수정완료!'})
 
+#레시피 상세보기
+@blue_recipe.route("/<under_id>")
+def detail(under_id):
 
+   id = db.recipes.find_one({'_id':ObjectId(under_id)})
+   reviews = list(db.review.find({'recipe_id':under_id}))
+   recipes = list(map(str, id['recipe'].split("\n")))
+
+   return render_template('recipe.html', id=id, reviews=reviews, recipes=recipes)
 
 
 # blue_recipe = Blueprint("main", __name__, url_prefix="/")
