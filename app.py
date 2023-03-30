be1f6b8cda1df1ab7e88d8c0ba7513517d83f141
ksnx3684 <ksnx3684@naver.com>
2023-03-30 17:38:23 +0900

    로그인 관련 일부 수정

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 # 전체 메인
-from flask import Flask
+from flask import Flask, render_template
 from route import recipe
 from route import review
 from route import user
 
-app = Flask(__name__)
+application = app = Flask(__name__)
 
 app.register_blueprint(user.blue_user)
 app.register_blueprint(recipe.blue_recipe)
 app.register_blueprint(review.blue_review)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 if __name__ == '__main__':  
-   app.run('0.0.0.0',port=5000,debug=True)
+   app.run()
